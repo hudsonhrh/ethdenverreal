6636a215e2ebc7965d93fb11056548c1ad1aa4e5
Emma Nasseri <enasseri02@gmail.com>
2023-02-28 10:47:49 -0700

    super basic cards taht look bad

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,37 +1,32 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import Logo from './Logo'
-import Links from './Links'
+import Logo from "./Logo";
+import Links from "./Links";
 
 const Container = styled.div.attrs({
-    className: 'container',
-})`width: 100%;`
+  className: "container",
+})`
+  width: 100%;
+`;
 
 const Nav = styled.nav.attrs({
-    className: 'navbar navbar-expand-lg ',
+  className: "navbar navbar-expand-lg ",
 })`
-    margin-bottom: 20 px;
-    width: 100%;
-    align-items: right;
-
-`
-
-
+  margin-bottom: 20 px;
+  width: 100%;
+  align-items: right;
+`;
 
 const NavBar = () => {
-        return (
-           
-                <Nav>
-                    
-                    <Logo />
-                    <Container fluid style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <Links style={{justifyContent: 'right' }}/>
-                    </Container>
-                </Nav>
-       
-        )
-    
-}
+  return (
+    <Nav>
+      <Logo />
+      <Container fluid style={{ display: "flex", justifyContent: "flex-end" }}>
+        <Links style={{ justifyContent: "right" }} />
+      </Container>
+    </Nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,10 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+import Stack from "react-bootstrap/Stack";
+import TaskCard from "../components/TaskCard";
+
+const Column = styled.div`
+  background: black;
+  float: left;
+  height: 100%;
+  width: 23%;
+  display: flex;
+  justify-content: center;
+  margin: 5%;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  display: table;
+  width: 100%;
+  height: 100%;
+`;
+
+const StackedColumn = styled(Stack)`
+  justifyContent: "center",
+  display: "flex",
+  alignItems: "center",
+`
 
 const Tasks = () => {
   return (
-    <div>
-      <p>Tasks</p>
-    </div>
+    <Wrapper>
+      <Column>
+        <Stack
+          style={{
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+        </Stack>
+      </Column>
+      <Column>
+        <Stack style={{
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+          }}>
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+        </Stack>
+      </Column>
+      <Column>
+        <Stack style={{
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+          }}>
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+          <TaskCard>task1</TaskCard>
+        </Stack>
+      </Column>
+    </Wrapper>
   );
 };
 
