793ee6065ea174493e0583bc358f05c3d445843f
Emma Nasseri <enasseri02@gmail.com>
2023-03-03 00:25:35 -0700

    created button lol

---
+++ client/src/components/CreateTask.jsx
@@ -0,0 +1,21 @@
+import temp_image from "../images/temp_image.png";
+import Button from "react-bootstrap/Button";
+import React from "react";
+
+function CreateTask() {
+  return (
+    <div
+      style={{
+        width: ""
+    
+      }}
+    >
+      
+      <h2>Create Task</h2>
+      <Button variant="primary">Create</Button>
+     
+    </div>
+  );
+}
+
+export default CreateTask;
