d8363d4161cc60ea5eaa66e0e207cd542481d50c
hudsonhrh <hudsonhrh7@gmail.com>
2023-03-02 23:08:45 -0700

    made like 4 smart contracts

--- contracts/MemberNFT.sol
+++ contracts/MemberNFT.sol
@@ -1,4 +1,3 @@
-//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
